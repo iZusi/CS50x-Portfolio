986587e4fbfffb6870baaf198ba9fa38a9c83289
Iyke <136558859+iZusi@users.noreply.github.com>
2023-09-28 01:34:45 +0100

    Create mario-more.c

---
+++ ProblemSets/Pset1/mario-more.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int i,j,k,l,m,o;
+
+int main(){
+    // prompt the user for an input between the range 1 < i > 8
+    // if input falls outside this range, reprompt user
+    do{
+        i = get_int("Height: ");
+    }
+    while (i < 1 || i > 8);
+
+    // create output based on user's input using a nested for loop
+    for(j = 0; j < i; j++){
+        for(k = i-1; k > j; k--){
+            printf(" ");
+        }
+
+        for(l = 0; l <= k; l++){
+            printf("#");
+        }
+
+        printf("  ");
+
+        for(m = 0; m < k; m++){
+            printf("#");
+        }
+        printf("#\n");
+    }
+}
